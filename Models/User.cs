86b79411214afe9ea44c260fdeba6380599bbd27
M4TTH3WW <matthewpascua22@gmail.com>
2024-10-17 16:31:33 +0800

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace LaundryHub.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string clWeight { get; set; }
+        public string status { get; set; }
+    }
+}
